57c0bf4a3dde6d0e525a897602cda12ee33a7083
Umesh <s.umesh@in.bosch.com>
2025-08-05 11:27:09 +0530

    build fix

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,37 @@
 #include "stats.h"
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+Stats Statistics::ComputeStatistics(const std::vector<float>& ) 
+{
     //Implement statistics here
+    ComputeStatistics(const std::vector<float>& data) {
+    Stats stats;
+    if (data.empty()) {
+        stats.min = 0.0f;
+        stats.max = 0.0f;
+        stats.mean = 0.0f;
+        stats.stddev = 0.0f;
+        return stats;
+    }
+    else {
+        float sum = 0.0f;
+        float sum_squared = 0.0f;
+        stats.min = data[0];
+        stats.max = data[0];
+
+        for (const auto& value : data) {
+            if (value < stats.min) {
+                stats.min = value;
+            }
+            if (value > stats.max) {
+                stats.max = value;
+            }
+            sum += value;
+            sum_squared += value * value;
+        }
+
+        stats.mean = sum / data.size();
+    }
+}
+
+    
 }
